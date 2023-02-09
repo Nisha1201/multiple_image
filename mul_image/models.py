1bd5c88310f40f272e2f4b153c8337939e882b4b
Nisha Sahu <sahunisha1010@gmail.com>
2023-02-09 14:19:01 +0530

    first commit

---
+++ mul_image/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+class MultipleImage(models.Model):
+    images = models.FileField()
---
+++ mul_image/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import MultipleImage
+def upload(request):
+    if request.method == "POST":
+        images = request.FILES.getlist('images')
+        for image in images:
+            MultipleImage.objects.create(images=image)
+    images = MultipleImage.objects.all()
+    return render(request, 'index.html', {'images': images})
